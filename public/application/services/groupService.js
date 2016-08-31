7c98eba10a00f3bce4dfb412ccd2093f5e6c1933
gustoss <gusto-s-s@hotmail.com>
2016-08-31 16:08:43 -0300

    Teste de back
    Verificar se o $location.host() funciona em produção

--- public/application/services/groupService.js
+++ public/application/services/groupService.js
@@ -2,14 +2,14 @@ angular.module("groupService", [])
 .service("groupAPI", function ($http) {
     this.checkUser  = function (username) {
         return $http({
-            url: "http://localhost:5000/api/v1/user/check/" + username,
+            url: "http://"+$location.host()+"/api/v1/user/check/" + username,
             method: "GET"
         });
     }
 
     this.saveGroup = function (infoGroup, user) {
         return $http({
-            url: "http://localhost:5000/api/v1/user",
+            url: "http://"+$location.host()+"/api/v1/user",
             method: "POST",
             data: { group: infoGroup, user: user }
         });
@@ -17,21 +17,21 @@ angular.module("groupService", [])
 
     this.getGroups = function () {
         return $http({
-            url: "http://localhost:5000/api/v1/user",
+            url: "http://"+$location.host()+"/api/v1/user",
             method: "GET"
         });
     };
 
     this.getGroup = function (id) {
         return $http({
-            url: "http://localhost:5000/api/v1/user/" + id,
+            url: "http://"+$location.host()+"/api/v1/user/" + id,
             method: "GET"
         });
     };
 
     this.update = function (infoGroup, id) {
         return $http({
-            url: "http://localhost:5000/api/v1/user/" + id,
+            url: "http://"+$location.host()+"/api/v1/user/" + id,
             method: "PUT",
             data: infoGroup
         });
