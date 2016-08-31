b45458cb56b14fb15bc757216df7b3befafc101d
gustoss <gusto-s-s@hotmail.com>
2016-08-31 13:01:53 -0300

    Add via web

--- routes/site.js
+++ routes/site.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
 	var ctrl = app.controllers.siteCtrl,
 		router = require("express").Router();
 
-	router.get("", ctrl.getHome);
+	router.get("/", ctrl.getHome);
 	router.get("/favicon.ico", ctrl.getFavicon);
 
 	app.use(app.config.pathWeb, router);
