563a80418de2f40e883f07a54055475800670d72
gustoss <gusto-s-s@hotmail.com>
2016-08-31 12:18:15 -0300

    Add via web
    Mongo connect to mLab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"secret": "%thatIsMyMusRentBestSiteEvery_#_S2S2_#_AndSomethingMore%",
-    "database": "mongodb://127.0.0.1/MUSREST",
+    "database": "mongodb://musrent_gusto:levado7musrent@ds019966.mlab.com:19966/musrent",
     "pathApi": "/api/v1",
     "pathWeb": "/",
     "pathPublic": __dirname + "/public"
